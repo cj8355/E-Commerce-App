1e35a4be711042055d45e203d952cef6ac0f361c
Charles Callender <charlescallender.cc@gmail.com>
2022-02-20 21:07:31 -0800

    Added cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
@@ -8,7 +9,7 @@ import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <Login />
+    <Cart />
   )
 };
 
