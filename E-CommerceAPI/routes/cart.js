caa3eb740ddc722463ca3bbf6769cca9fa946048
Charles Callender <charlescallender.cc@gmail.com>
2022-02-23 06:01:16 -0800

    Added routes

--- E-CommerceAPI/routes/cart.js
+++ E-CommerceAPI/routes/cart.js
@@ -1,5 +1,69 @@
+const Cart = require("../models/Cart");
+const Product    = require("../models/Product");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
+// Create
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newCart = new Cart(req.body)
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedProduct);
+    } catch(err) {
+        res.status(500).json(err);
+    };
+});
+
+// Update
+router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
+    
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new:true}
+        );
+        res.status(200).json(updatedCart);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Delete user
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Cart deleted");
+    } catch(err) {
+        res.status(500).json(err)
+    }
+});
+
+// Get user cart
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req,res) => {
+    try {
+        const Cart = await Cart.findOne({userId: req.params.userId});
+        res.status(200).json(cart);
+    } catch(err) {
+        res.status(500).json(err)
+    }
+});
+
+// Get All
+
+router.get("/", verifyTokenAndAdmin, async (req,res) => {
+    try {
+
+        const carts = await Cart.find();
+        res.status(200).json(carts);
+
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
 
 
 module.exports = router;
--- E-CommerceAPI/routes/order.js
+++ E-CommerceAPI/routes/order.js
@@ -1,5 +1,71 @@
+const Order = require("../models/Order");
+const Product    = require("../models/Product");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
+// Create
 
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newOrder = new Order(req.body)
+
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    } catch(err) {
+        res.status(500).json(err);
+    };
+});
+
+// Update
+router.put("/:id", verifyTokenAndAdmin, async (req,res) => {
+    
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new:true}
+        );
+        res.status(200).json(updatedOrder);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Delete order
+router.delete("/:id", verifyTokenAndAdmin, async (req,res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Order deleted");
+    } catch(err) {
+        res.status(500).json(err)
+    }
+});
+
+// Get user orders
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req,res) => {
+    try {
+        const orders = await Order.find({userId: req.params.userId});
+        res.status(200).json(orders);
+    } catch(err) {
+        res.status(500).json(err)
+    }
+});
+
+// Get All
+
+router.get("/", verifyTokenAndAdmin, async (req,res) => {
+    try {
+
+        const orders = await Order.find();
+        res.status(200).json(orders);
+
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+
+// Get Monthly income
 
 module.exports = router;
