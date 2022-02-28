73e5961bc6820f2dce4e50a3a44ddba97b490d6b
Charles Callender <charlescallender.cc@gmail.com>
2022-02-27 16:31:47 -0800

    Added admin dashboard

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -4,10 +4,10 @@ const BASE_URL = "http://localhost:5000/api/";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
-    header: {token: `Bearer ${TOKEN}`}
-})
+  baseURL: BASE_URL,
+  headers: { token: `Bearer ${TOKEN}` },
+});
