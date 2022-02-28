19ae8d7788893dbd88701fb84b74391bc52e98f7
Charles Callender <charlescallender.cc@gmail.com>
2022-02-27 18:01:57 -0800

    Added firebase

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -5,15 +5,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCB-h_T2ZL-a7xt5zMvZmSh8GBXtawzL0c",
-  authDomain: "shop-889c6.firebaseapp.com",
-  projectId: "shop-889c6",
-  storageBucket: "shop-889c6.appspot.com",
-  messagingSenderId: "105110075028",
-  appId: "1:105110075028:web:7cee5adf1ed39f597e6681",
+  apiKey: "AIzaSyCRalo6iWCjtd4sRhdTVwom9srvwsesNIA",
+  authDomain: "shop-22245.firebaseapp.com",
+  projectId: "shop-22245",
+  storageBucket: "shop-22245.appspot.com",
+  messagingSenderId: "754897396067",
+  appId: "1:754897396067:web:9d6ab7ba9bba0c068e7356"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 export default app;
